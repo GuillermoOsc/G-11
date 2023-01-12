0c3ba6a0c8df98b2b547606c45cd89d92ee4f7d0
Guillermo Oscar <85650228+GuillermoOsc@users.noreply.github.com>
2023-01-12 01:07:50 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+## Testcafe-Js
+
+### Automatización de pruebas para testing utilizando el framework Testcafe Js.
+
+### Modulos utilizados
+
+- dotenv: Módulo para variables de entorno.
+- testcafe: Framework de automatización.
+- testcafe-reporter-html: Módulo para reportes de resultados.
---
+++ pom/pages/loginPage.js
@@ -0,0 +1,32 @@
+// Mediante el constructor se reduce la cantidad de codigo y se agiliza el uso de diferentes páginas para seleccionar los elementos
+// Este patrón de diseño se denomina POM (Page Objet Model)
+
+
+
+import { Selector, t } from "testcafe";
+
+class loginPage {
+    constructor(){
+        this.user = Selector('#user-name')
+        this.pass = Selector('input[data-test="password"]')
+        this.button = Selector('input[name="login-button"]')
+        this.title = Selector('h3')
+    }
+
+
+// Se valida si los campos user y password son nulos para establecer happypath y escenarios negativos en el folder loginTest.js
+
+    async submitLoginForm(user, pass) {
+        if(user != null){
+            await t.typeText(this.user, user)
+        }
+        if(pass != null){
+            await t.typeText(this.pass, pass)
+        }
+        await t.click(this.button)
+    }
+
+};
+
+
+export default new loginPage
