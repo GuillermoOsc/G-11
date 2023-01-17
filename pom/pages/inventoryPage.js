e9b861d87e12eec2727763c57997609637e544a5
Guillermo Oscar <guillermobianco9090@gmail.com>
2023-01-17 00:49:33 -0300

    Se valida el ingreso al sitio tomando c/ referencia el titulo

--- pom/pages/inventoryPage.js
+++ pom/pages/inventoryPage.js
@@ -2,8 +2,9 @@ import { Selector } from 'testcafe'
 
 class inventoryPage {
     constructor(){
-        this.title = Selector('.title').withText('PRODUCTS')
+        this.title = Selector('.AppBar_tituloHeader__6AM7r').withText('Melinda Muriel')
     }
 }
 
 export default new inventoryPage
+
