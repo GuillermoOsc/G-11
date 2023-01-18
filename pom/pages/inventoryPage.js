2649def4e0a7f84fb305da91689d09bf82feb88b
Guillermo Oscar <guillermobianco9090@gmail.com>
2023-01-17 23:37:04 -0300

    TC registro de nuevo usuario

--- pom/pages/inventoryPage.js
+++ pom/pages/inventoryPage.js
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe'
 
 class inventoryPage {
     constructor(){
-        this.title = Selector('.AppBar_tituloHeader__6AM7r').withText('Melinda Muriel')
+        this.title = Selector('.AppBar_tituloHeader__6AM7r').withText('Melinda Muriel') 
     }
 }
 
